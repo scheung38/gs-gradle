32b59c179c06b3d79f9a688d2cb311edcb5e73b9
Sebastian Cheung <sebastian_cheung@live.com>
2020-09-17 23:41:13 +0100

    Added lombok, concatenate

--- complete/src/main/java/hello/Greeter.java
+++ complete/src/main/java/hello/Greeter.java
@@ -1,7 +1,62 @@
 package hello;
+import java.security.MessageDigest;
+import java.nio.charset.StandardCharsets;
+import java.math.BigInteger; 
+import java.security.NoSuchAlgorithmException;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+// @EqualsAndHashCode
+@Data
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Greeter {
-  public String sayHello() {
-	return "Hello world!";
-  }
+
+    private int reportingYear;
+    private int reportingMonth;
+    private String businessUnitId;
+
+    public String concatenate(String... s) { //static 
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < s.length; i++) {
+      sb = sb.append(s[i]);
+    }
+      return sb.toString();
+    }
+   
+
+    public byte[] getSHA(String input) throws NoSuchAlgorithmException 
+    {  
+        // Static getInstance method is called with hashing SHA  
+        MessageDigest md = MessageDigest.getInstance("MD5");  
+  
+        // digest() method called  
+        // to calculate message digest of an input  
+        // and return array of byte 
+        return md.digest(input.getBytes(StandardCharsets.UTF_8));  
+    } 
+    
+    public String toHexString(byte[] hash) 
+    { 
+        // Convert byte array into signum representation  
+        BigInteger number = new BigInteger(1, hash);  
+  
+        // Convert message digest into hex value  
+        StringBuilder hexString = new StringBuilder(number.toString(16));  
+  
+        // Pad with leading zeros 
+        while (hexString.length() < 32)  
+        {  
+            hexString.insert(0, '0');  
+        }  
+  
+        return hexString.toString();  
+    } 
+
 }
--- complete/src/main/java/hello/HelloWorld.java
+++ complete/src/main/java/hello/HelloWorld.java
@@ -1,13 +1,46 @@
 package hello;
-
+import java.security.MessageDigest;
+import java.nio.charset.StandardCharsets;
+import java.math.BigInteger; 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.joda.time.LocalTime;
 
 public class HelloWorld {
   public static void main(String[] args) {
+
 	LocalTime currentTime = new LocalTime();
 	System.out.println("The current local time is: " + currentTime);
 
 	Greeter greeter = new Greeter();
-	System.out.println(greeter.sayHello());
+    
+        try {
+	
+          greeter.setReportingYear(2020);
+          String year = Integer.toString(greeter.getReportingYear());
+          // String year = String.valueOf(greeter.getReportingYear());
+           
+          greeter.setReportingMonth(7);
+          String month = Integer.toString(greeter.getReportingMonth());
+
+          greeter.setBusinessUnitId("SD-CLAIMS-01");
+          String businessUnitId = greeter.getBusinessUnitId();
+
+			    String fte = greeter.concatenate(new String[]{year, "~", month, "~", businessUnitId});
+		      System.out.println("fte: " + fte);
+          System.out.println("\n" + fte + " : " + greeter.toHexString(greeter.getSHA(fte)));  
+           
+            // 2020~7~SD-CLAIMS-01 : 7ba5f729d20b610d517fa0f222389cf9
+            // 2019~7~SD-CLAIMS-01 : 9a6b95187a92341aa885e66585c23532
+            // 2018~7~SD-CLAIMS-01 : e8a894c29786292f4014ec5a14081ba9
+
+        } catch (Exception e) { //NoSuchAlgorithmException
+             System.out.println("Something is wrong");
+            
+        }
   }
 }
